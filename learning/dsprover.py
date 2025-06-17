156bc20442f82b705018cd2fbd8905172bca657b
tkasriel <timothe@kasriel.com>
2025-06-16 19:59:38 -0700

    switched to VLLM

--- learning/dsprover.py
+++ learning/dsprover.py
@@ -2,7 +2,7 @@ import json
 import re
 import time
 import tqdm
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from vllm import LLM, SamplingParams
 import torch
 from accelerate import PartialState
 
@@ -10,13 +10,14 @@ from lean_interact import LeanREPLConfig, AutoLeanServer, Command
 torch.manual_seed(30)
 
 model_id = "deepseek-ai/DeepSeek-Prover-V2-7B"
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-distributed_state = PartialState()
-local_rank = distributed_state.local_process_index
-device = torch.device(f"cuda:{local_rank}")
-model = AutoModelForCausalLM.from_pretrained(model_id, device_map={"":device}, torch_dtype=torch.bfloat16, trust_remote_code=True)
+model = LLM(model=model_id)
+tokenizer = model.get_tokenizer()
+# tokenizer = AutoTokenizer.from_pretrained(model_id)
+# distributed_state = PartialSstate()
+# local_rank = distributed_state.local_process_index
+# device = torch.device(f"cuda:{local_rank}")
+# model = AutoModelForCausalLM.from_pretrained(model_id, device_map={"":device}, torch_dtype=torch.bfloat16, trust_remote_code=True)
 server = AutoLeanServer(LeanREPLConfig())
-
 
 def extract_code (encoded_response, logprobs, debug = False) -> tuple[str | None, float]:
     code_blocks: list[list[int]] = []
@@ -26,19 +27,19 @@ def extract_code (encoded_response, logprobs, debug = False) -> tuple[str | None
     for i, token in enumerate(encoded_response):
         if code_block_open:
             code_blocks[-1].append(token)
-            logprobs_out[-1] += logprobs[i]
+            logprobs_out[-1] += logprobs[i][token].logprob
         if token == 10897:
             code_block_open = not code_block_open
             if code_block_open:
                 code_blocks.append([token])
-                logprobs_out.append(logprobs[i])
+                logprobs_out.append(logprobs[i][token].logprob)
     code_blocks = [tokenizer.decode(code_block) for code_block in code_blocks]
     paired = zip(code_blocks, logprobs_out)
     paired = list(filter(lambda x : "lean4" in x[0], paired))
     paired.sort(key=lambda x : len(x[0]))
     if len(paired) == 0:
         return None, 0.0
-    return (re.sub(r"\n(.*?<;>.*?\n)","\n", paired[-1][0].replace("```", "").replace("lean4", "")).strip(), float(paired[-1][1]))
+    return (paired[-1][0].replace("```", "").replace("lean4", "").strip(), float(paired[-1][1]))
 
 
 def prove (conjectures : list[str], debug: bool = False) -> list[tuple[str | None, float]]:
@@ -62,25 +63,26 @@ def prove (conjectures : list[str], debug: bool = False) -> list[tuple[str | Non
         ])
     if debug:
         start_time = time.time()
+    
 
-    texts = tokenizer.apply_chat_template(input_texts, tokenize=False, add_generation_prompt=True)
-    tokenizer.pad_token_id = tokenizer.eos_token_id
-    inputs = tokenizer(texts, padding="longest", return_tensors="pt", verbose=False)
-    inputs = {key: val.to(device) for key, val in inputs.items()}
-    if debug:
-        print(f"tokenization took {time.time() - start_time}s")
-        start_time = time.time()
+    # texts = tokenizer.apply_chat_template(input_texts, tokenize=False, add_generation_prompt=True)
+    # tokenizer.pad_token_id = tokenizer.eos_token_id
+    # inputs = tokenizer(texts, padding="longest", return_tensors="pt", verbose=False)
 
-    outputs = model.generate(**inputs, max_new_tokens=8192, return_dict_in_generate=True, output_scores=True)
-    input_lengths = inputs["input_ids"].shape[1]
-    transition_scores = model.compute_transition_scores (outputs.sequences, outputs.scores, normalize_logits=True)
-    generated_tokens = outputs.sequences[:,input_lengths:]
+    # inputs = {key: val.to(device) for key, val in inputs.items()}
+    sample_params = SamplingParams(max_tokens=8192, logprobs=0, detokenize=False)
+    model_results = model.chat(input_texts, sample_params, use_tqdm=True)
+    generated_tokens = [out.outputs[0].token_ids for out in model_results]
+    logprobs = [out.outputs[0].logprobs for out in model_results]
+
+    # outputs = model.generate(**inputs, max_new_tokens=8192, return_dict_in_generate=True, output_scores=True)
+
     if debug:
-        print (f"Inference/decoding took {time.time()-start_time}s")
+        print (f"Inference took {time.time()-start_time}s")
         start_time = time.time()
 
     results = []
-    for generated_i, transition_i in zip(generated_tokens, transition_scores):
+    for generated_i, transition_i in zip(generated_tokens, logprobs):
         lean_code, logprob = extract_code(generated_i, transition_i, debug=debug)
         if not lean_code:
             results.append((None, 0.0))
@@ -117,10 +119,10 @@ if __name__ == "__main__":
     # print (f"Proving {num_conj} conjectures took {mid_time - start_time}s unbatched")
     batches = [true_theorems[i:min(i+batch_size, len(true_theorems))] for i in range(0, len(true_theorems), batch_size)]
     bar = tqdm.tqdm(total=len(true_theorems))
-    with distributed_state.split_between_processes(batches) as batch:
+    for batch in batches:
         prove(batch, debug=True)
         bar.update(len(batch))
-    bar.close()
+        bar.close()
     # for i in range(0, len(true_theorems), batch_size):
     #     print (f"it {i}/{len(true_theorems)}")
     #     batch = true_theorems[i:min(i+batch_size, len(true_theorems))]
